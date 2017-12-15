e07a32666ae5572b29de3d73847ebc063cc2eb07
Nombre <x.zenteno.a@gmail.com>
2017-12-15 20:07:17 -0300

    Primer Commit Se inicializa programa demowhile

---
+++ src/demowhile/DemoWhile.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demowhile;
+
+/**
+ *
+ * @author Concepción
+ */
+public class DemoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Equivalente a un "for". Imprime de 0 a 9
+        int i = 10;
+        while(i < 10) {
+            if (i % 2 == 0) {
+                System.out.println("El numero " + i + " es par");
+            } else {
+                System.out.println("El numero " + i + " es impar");
+            }
+            i++;
+        }
+        
+        // "do-while" equivalete a un "for". Imprime de 0 a 9
+        // Ejecutara AL MENOS UNA VEZ
+        int k = 10;
+        do {
+            System.out.println(k);
+            k++;
+        } while(k < 10);
+    }
+    
+}
